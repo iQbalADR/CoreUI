b233c8c2b3707b3288ef368651600adcaf223a82
Iqbal Adriansyah <Iqbal.adriansyah@bankmandiri.co.id>
2019-09-05 16:46:36 +0700

    add button error

---
+++ CoreUI/CoreUI/ButtonError.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonError.swift
+//  CoreUI
+//
+//  Created by IQ on 05/09/19.
+//  Copyright © 2019 Iqbal Adriansyah Adrial. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class ButtonError: UIButton {
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
---
+++ CoreUI/CoreUI/ButtonViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonViewController.swift
+//  CoreUI
+//
+//  Created by IQ on 05/09/19.
+//  Copyright © 2019 Iqbal Adriansyah Adrial. All rights reserved.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    @IBOutlet weak var buttonError: ButtonError!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
